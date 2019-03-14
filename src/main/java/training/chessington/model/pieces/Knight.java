6a843e754e19be9373c69f0b4e33d91b4eb790e7
Hannah.Mayhew <hannah.mayhew@HannahMayhews-MacBook-Pro.local>
2019-03-14 15:55:19 +0000

    Working Knight

--- src/main/java/training/chessington/model/pieces/Knight.java
+++ src/main/java/training/chessington/model/pieces/Knight.java
@@ -6,6 +6,7 @@ import training.chessington.model.Move;
 import training.chessington.model.PlayerColour;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Knight extends AbstractPiece {
@@ -15,6 +16,33 @@ public class Knight extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> allowedMoves = new ArrayList<>();
+        List<Move> potentialMoves = Arrays.asList(
+            new Move(from, from.plus(-2, 1)),
+            new Move(from, from.plus(-1, 2)),
+            new Move(from, from.plus(1, 2)),
+            new Move(from, from.plus(2, 1)),
+            new Move(from, from.plus(2, -1)),
+            new Move(from, from.plus(1, -2)),
+            new Move(from, from.plus(-1, -2)),
+            new Move(from, from.plus(-2, -1))
+        );
+
+        for (Move move : potentialMoves) {
+            if (moveIsAllowed(move, board)) {
+                allowedMoves.add(move);
+            }
+        }
+        return allowedMoves;
+    }
+
+    private boolean moveIsAllowed(Move move, Board board) {
+        boolean moveIsAllowed = false;
+        if (move.getTo().getRow() >= 0 && move.getTo().getRow() <= 7 && move.getTo().getCol() >= 0 && move.getTo().getCol() <= 7) {
+            if (board.get(move.getTo()) == null || board.get(move.getTo()).getColour() != colour) {
+                moveIsAllowed = true;
+            }
+        }
+        return moveIsAllowed;
     }
 }
--- src/test/java/training/chessington/model/pieces/KnightTest.java
+++ src/test/java/training/chessington/model/pieces/KnightTest.java
@@ -1,7 +1,78 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class KnightTest {
+    @Test
+    public void whiteKnightCanMoveInLShape() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates knightCoords = new Coordinates(5, 2);
+        board.placePiece(knightCoords, knight);
+
+        //Act
+        List<Move> moves = knight.getAllowedMoves(knightCoords, board);
+
+        //Asserts
+        assertThat(moves).contains(new Move(knightCoords, knightCoords.plus(-2, 1)));
+        assertThat(moves).contains(new Move(knightCoords, knightCoords.plus(-1, 2)));
+        assertThat(moves).contains(new Move(knightCoords, knightCoords.plus(1, 2)));
+        assertThat(moves).contains(new Move(knightCoords, knightCoords.plus(2, 1)));
+        assertThat(moves).contains(new Move(knightCoords, knightCoords.plus(2, -1)));
+        assertThat(moves).contains(new Move(knightCoords, knightCoords.plus(1, -2)));
+        assertThat(moves).contains(new Move(knightCoords, knightCoords.plus(-1, -2)));
+        assertThat(moves).contains(new Move(knightCoords, knightCoords.plus(-2, -1)));
+    }
+    @Test
+    public void whiteKnightCanNotBeMovedOffLeftOrBottomOfBoard() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates knightCoords = new Coordinates(7, 1);
+        board.placePiece(knightCoords, knight);
+
+        //Act
+        List<Move> moves = knight.getAllowedMoves(knightCoords, board);
+
+        //Asserts
+        assertThat(moves).containsExactlyInAnyOrder(
+            new Move(knightCoords, knightCoords.plus(-2, -1)),
+            new Move(knightCoords, knightCoords.plus(-2, 1)),
+            new Move(knightCoords, knightCoords.plus(-1, 2))
+        );
+    }
+
+    @Test
+    public void blackKnightCanNotBeMovedOffTopOrRightOfBoard() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates knightCoords = new Coordinates(0, 6);
+        board.placePiece(knightCoords, knight);
+
+        //Act
+        List<Move> moves = knight.getAllowedMoves(knightCoords, board);
+
+        //Asserts
+        assertThat(moves).containsExactlyInAnyOrder(
+                new Move(knightCoords, knightCoords.plus(2, 1)),
+                new Move(knightCoords, knightCoords.plus(2, -1)),
+                new Move(knightCoords, knightCoords.plus(1, -2))
+        );
+    }
+
+    @Test
+    public void knightCannotEnterSpaceOccupiedByOwnPiece() {
+
+    }
 }
