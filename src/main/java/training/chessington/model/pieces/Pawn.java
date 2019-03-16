ce68e4d90835470fd0fda597000c58c918f5d53a
Hannah.Mayhew <hannah.mayhew@hannahmyhewsmbp.mynet>
2019-03-16 16:36:03 +0000

    refactored pawn

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -39,55 +39,6 @@ public class Pawn extends AbstractPiece {
         if (MoveUtilities.moveIsAllowed(captureLeft, board, this) && board.get(captureLeft.getTo()) != null) {
             allowedMoves.add(captureLeft);
         }
-
-
-
-
-        /*if (colour.equals(PlayerColour.WHITE) && from.getRow() != 0) {
-            if (from.getCol() != 7) {
-                Piece diagRight = board.get(from.plus(-1, 1));
-                if (diagRight != null && diagRight.getColour() == PlayerColour.BLACK) {
-                    allowedMoves.add(new Move(from, from.plus(-1, 1)));
-                }
-            }
-            if (from.getCol() != 0) {
-                Piece diagLeft = board.get(from.plus(-1, -1));
-                if (diagLeft != null && diagLeft.getColour() == PlayerColour.BLACK) {
-                    allowedMoves.add(new Move(from, from.plus(-1, -1)));
-                }
-            }
-            if (board.get(from.plus(-1, 0)) == null) { // if space in front is empty
-                if (from.getRow() == 6) {
-                    if (board.get(from.plus(-2,0)) == null) {
-                        allowedMoves.add(new Move(from, from.plus(-2, 0)));
-                    }
-                }
-                allowedMoves.add(new Move(from, from.plus(-1, 0)));
-            }
-        }
-
-        if (colour.equals(PlayerColour.BLACK) && from.getRow() != 7) {
-            if (from.getCol() != 7) {
-                Piece diagRight = board.get(from.plus(1, 1));
-                if (diagRight != null && diagRight.getColour() == PlayerColour.WHITE) {
-                    allowedMoves.add(new Move(from, from.plus(1, 1)));
-                }
-            }
-            if (from.getCol() != 0) {
-                Piece diagLeft = board.get(from.plus(1, -1));
-                if (diagLeft != null && diagLeft.getColour() == PlayerColour.WHITE) {
-                    allowedMoves.add(new Move(from, from.plus(1, -1)));
-                }
-            }
-            if (board.get(from.plus(1, 0)) == null) {
-                if (board.get(from.plus(2,0)) == null) {
-                    if (from.getRow() == 1) {
-                        allowedMoves.add(new Move(from, from.plus(2, 0)));
-                    }
-                }
-                allowedMoves.add(new Move(from, from.plus(1, 0)));
-            }
-        }*/
         return allowedMoves;
     }
 }
